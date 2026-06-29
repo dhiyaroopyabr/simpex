1864246f360fc3c99f277647564715f740579775
dhiyaroopyabr <dhiyaroopyabr@gmail.com>
2026-06-29 17:04:17 +0530

    icon display

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
-const CACHE_NAME = 'simpex-v1';
+const CACHE_NAME = 'simpex-v2';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
   './icons/icon-512.png'
 ];
 
