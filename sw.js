cecbd23603f91f8ca766322cb1d81671acdc0066
dhiyaroopyabr <dhiyaroopyabr@gmail.com>
2026-06-29 17:31:26 +0530

    trial

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'simpex-v2';
+const CACHE_NAME = 'simpex-v3';
 const ASSETS = [
   './',
   './index.html',
@@ -10,6 +10,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
@@ -18,6 +19,7 @@ self.addEventListener('activate', (event) => {
       Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
     )
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
